0685b68dd6d6d9ac3b82109bdab93a03f07837e8
FDStation <FDStation@users.noreply.github.com>
2015-08-06 10:56:48 +0800

    2015/8/6 大小写真坑爹 真麻烦

---
+++ src/Common/FileUtils.cpp
@@ -0,0 +1,33 @@
+#include "FileUtils.h"
+#include "Windows.h"
+
+void getRootPath(char* utf8Path, size_t size)
+{
+	WCHAR utf16Path[256] = {0};
+	GetCurrentDirectoryW(sizeof(utf16Path)-1, utf16Path);
+
+	int nNum = WideCharToMultiByte(CP_UTF8, 0, utf16Path, -1, utf8Path, size, nullptr, nullptr);
+
+	convertPathFormatToUnixStyle(utf8Path);
+}
+
+void convertPathFormatToUnixStyle(char* path)
+{
+	int len = strlen(path);
+	for (int i = 0; i != len; ++i)
+	{
+		if (*(path + i) == '\\')
+		{
+			*(path + i) = '/';
+		}
+	}
+}
+
+void getFullPath(const char* filePath, char* fullPath, size_t size)
+{
+	char path[256] = {0};
+
+	getRootPath(path, sizeof(path));
+
+	sprintf_s(fullPath, size, "%s/%s", path, filePath);
+}
